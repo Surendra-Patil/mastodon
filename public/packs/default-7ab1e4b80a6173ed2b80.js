1d38b74405cd0fcf1a4baf7bf59dbe29b5022e49
surendra <meetsurendrapatil@gmail.com>
2018-07-04 15:03:35 +0530

    Mastodon working version

---
+++ public/packs/default-7ab1e4b80a6173ed2b80.js
@@ -0,0 +1,2 @@
+webpackJsonp([74],{786:function(n,c){}},[786]);
+//# sourceMappingURL=default-7ab1e4b80a6173ed2b80.js.map
---
+++ public/packs/mailer-1e0a88525b8433fa1206.js
@@ -0,0 +1,2 @@
+webpackJsonp([73],{675:function(n,c,o){o(676)},676:function(n,c){}},[675]);
+//# sourceMappingURL=mailer-1e0a88525b8433fa1206.js.map
